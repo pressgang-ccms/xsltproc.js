04f1726f243a640e73c395d9958c8899dda5ddc2
Matthew Casperson <matthewcasperson@gmail.com>
2014-01-06 09:17:14 +1000

    Update pre.js
    Fixed issue where the Module['preRun'] property was being overwritten, which was preventing runWithFS() from being called.

--- pre.js
+++ pre.js
@@ -1,8 +1,13 @@
-Module['preRun'] = function() {
+if (!Module['preRun'])
+{
+	Module['preRun'] = [];
+}
+Module['preRun'].push(function() {
     FS.createDataFile('/', 'topic.xml', Module['intArrayFromString'](Module['xml']), true, false);
-};
+});
 Module.arguments = ['html/docbook.xsl', 'topic.xml'];
 Module['return'] = '';
+
 Module['print'] = function(text) {
   Module['return'] += text + '\n';
 };
