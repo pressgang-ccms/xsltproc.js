de5fda0fa13d73d51790a1d39d89efa79add8bc4
Matthew Casperson <matthewcasperson@gmail.com>
2014-01-15 09:36:56 +1000

    Update xmllint-pre.js

--- xmllint-pre.js
+++ xmllint-pre.js
@@ -2,10 +2,26 @@ if (!Module['preRun']) {
 	Module['preRun'] = [];
 }
 Module['preRun'].push(function() {
-    FS.createDataFile('/', 'topic.xml', Module['intArrayFromString'](Module['xml']), true, true);
-    FS.createDataFile('/', 'docbook45.dtd', Module['intArrayFromString'](Module['schema']), true, true);
+    	FS.createDataFile('/', 'topic.xml', Module['intArrayFromString'](Module['xml']), true, true);
+    	if (Module['schema'])
+	{
+		FS.createDataFile('/', 'docbook45.dtd', Module['intArrayFromString'](Module['schema']), true, true);
+	}
+	else if (Module['nrg'])
+	{
+		FS.createDataFile('/', 'docbook5.nrg', Module['intArrayFromString'](Module['nrg']), true, true);
+	}
 });
-Module.arguments = ["--noout", "--dtdvalid", "docbook45.dtd", "topic.xml"];
+
+if (Module['schema'])
+{
+	Module.arguments = ["--noout", "--dtdvalid", "docbook45.dtd", "topic.xml"];
+}
+else if (Module['nrg'])
+{
+	Module.arguments = ["--noout", "--relaxng", "docbook5.nrg", "topic.xml"];
+}
+
 Module['return'] = '';
 Module['print'] = function(text) {
 	Module['return'] += text + '\n';
