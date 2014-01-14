a6e98946ca7c1f9e8dc4be82cbc378b5ea826589
Matthew Casperson <matthewcasperson@gmail.com>
2014-01-15 09:48:12 +1000

    Update final.sh

--- final.sh
+++ final.sh
@@ -11,14 +11,14 @@ echo "  return Module.return;" >> xsltproc.js
 echo "}  " >> xsltproc.js
 
 echo "self.addEventListener('message', function (e) {" > xmllint.js
-echo "	postMessage(validateXML(e.data.xml, e.data.schema, e.data.nrg));" >> xmllint.js
+echo "	postMessage(validateXML(e.data.xml, e.data.schema, e.data.rng));" >> xmllint.js
 echo "});" >> xmllint.js
 
-echo "function validateXML(xml, schema, nrg) {" >> xmllint.js
+echo "function validateXML(xml, schema, rng) {" >> xmllint.js
 echo "  var Module = {" >> xmllint.js
 echo "    xml: xml," >> xmllint.js
 echo "    schema: schema," >> xmllint.js
-echo "    nrg: nrg" >> xmllint.js
+echo "    rng: rng" >> xmllint.js
 echo "  };" >> xmllint.js
 cat xmllint.raw.js >> xmllint.js
 echo "  return Module.return;" >> xmllint.js
